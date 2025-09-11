1d0335819d6797d4634ada7a4c8355f7025510c4
oleksiy228174 <olexkhaynatskiy@gmail.com>
2025-09-11 19:02:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+body{
+  font-family: 'Inter', sans-serif;
+  background: ‎#ffffff‎;
+  color: #333;
+  margin: 0;
+  padding: 0;
+}
 
+.site-header {
+  display: flex;
+  justify-content: space-between;
+  alighn-items: center;
+  background-color:#EBFFC2;
+  padding: 10 px, 40px;
+  flex-wrap: wrap;
+}
+.logo {
+  width: 120px;
+  height: 120px;
+  background-image: url('https://i.postimg.cc/FKHtq7k2/TF-2.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.logo-block {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
